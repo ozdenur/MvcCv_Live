45496d7d6bab1b0bc21e122af6be76a7f328ecaa
ozdenur <121803083+ozdenur@users.noreply.github.com>
2023-06-07 11:51:48 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;  
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DBCvEntities3 db=new DBCvEntities3();
+        public ActionResult Index()
+        {
+            var degerler= db.TblHakkimda.ToList();
+            return View(degerler);
+        }  
+        public PartialViewResult Deneyim()
+        {
+            return PartialView();
+        }
+    }
+} 
